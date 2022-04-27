9d2579402eec43e0ea17a2d9e6881950c4e0e77a
Erick Fuga <erickcfuga@gmail.com>
2022-04-27 13:54:58 -0300

    move config loading to module

--- configs/app/app_config_module.go
+++ configs/app/app_config_module.go
@@ -1,32 +1,33 @@
 package app
 
 import (
-	"context"
 	"errors"
 
-	. "go.uber.org/fx"
+	"go.uber.org/fx"
 
 	"github.com/spf13/viper"
 )
 
-var Invokables = Invoke(InvokeAppConfigs)
+var Module = fx.Provide(
+	// This enables you to inject Viper as a provider in case you want to
+	// use your configs as a service.
+	func() (*viper.Viper, error) {
+		if err := viper.BindEnv("app.env", "APP_ENV"); err != nil {
+			return nil, err
+		}
 
-func InvokeAppConfigs(lifecycle Lifecycle) {
-	lifecycle.Append(Hook{
-		OnStart: func(context.Context) error {
-			if err := viper.BindEnv("app.env", "APP_ENV"); err != nil {
-				return err
-			}
+		if !viper.IsSet("app.env") {
+			return nil, errors.New("The APP_ENV variable must be set!")
+		}
 
-			if !viper.IsSet("app.env") {
-				return errors.New("The APP_ENV variable must be set!")
-			}
+		viper.SetConfigName(viper.GetString("app.env"))
+		viper.SetConfigType("yaml")
+		viper.AddConfigPath("./configs/app")
 
-			viper.SetConfigName(viper.GetString("app.env"))
-			viper.SetConfigType("yaml")
-			viper.AddConfigPath("./configs/app")
+		if err := viper.MergeInConfig(); err != nil {
+			return nil, err
+		}
 
-			return viper.MergeInConfig()
-		},
-	})
-}
+		return viper.GetViper(), nil
+	},
+)
--- configs/config_module.go
+++ configs/config_module.go
@@ -1,22 +1,12 @@
 package configs
 
 import (
-	. "go.uber.org/fx"
+	"go.uber.org/fx"
 
-	"github.com/spf13/viper"
-	"github.com/thefuga/go-template/configs/app"
+	"github.com/thefuga/go-poc/configs/app"
 )
 
 var (
-	Module = Provide(
-		// This enables you to inject Viper as a provider in case you want to
-		// use your configs as a service.
-		func() *viper.Viper {
-			return viper.GetViper()
-		},
-	)
-
-	Invokables = Options(
-		app.Invokables,
-	)
+	Module     = fx.Options(app.Module)
+	Invokables = fx.Options()
 )
